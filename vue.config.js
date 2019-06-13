1d68f56370169307232f00cc8d99ff9df76e6915
Vadim Kochergin <tamato2008@rambler.ru>
2019-06-13 11:25:29 +0300

    update build config, add bundle analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,18 @@
-module.exports = {}
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const isDev = process.env.NODE_ENV !== 'production'
+
+const externals = {
+    "vue-multiselect": "vue-multiselect",
+}
+
+const plugins = []
+
+if(!isDev) plugins.push(new BundleAnalyzerPlugin)
+
+
+module.exports = {
+    configureWebpack: {
+        externals: isDev ? {} : externals,
+        plugins: plugins
+    }
+}
